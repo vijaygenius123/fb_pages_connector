c6188bd0c353d3cdc7dd70584d3ee7f94c27ef3d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-27 08:22:21 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/facebook/fb_pages_connector/FbPagesConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.facebook.fb_pages_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbPagesConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbPagesConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/facebook/fb_pages_connector/FbPagesConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.facebook.fb_pages_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbPagesConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
